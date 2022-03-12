69887cd544eec26f831b6237e6efca5da46dd208
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-12 18:14:07 +0530

    Prime Number Program

---
+++ LogicalPrograms3/src/UC3_PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class UC3_PrimeNumber {
+	void prime(int number) {
+		boolean primeNumber=true;
+		for(int i=2;i<number;i++) {
+			if(number%i==0) {
+
+				primeNumber=false;
+				break;
+				}
+
+		}
+		if(primeNumber==true) {
+			System.out.println("Its is a prime Number");
+		}
+		else {
+			System.out.println("Ti is not a prime Number");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner inp=new Scanner(System.in);
+		UC3_PrimeNumber rv=new UC3_PrimeNumber();
+		System.out.println("Enter a Number ");
+		int number=inp.nextInt();
+		rv.prime(number);
+	}
+
+}
