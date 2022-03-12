a02c6ea95dbb5801808cb76fefa67afc94226f50
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-12 18:19:33 +0530

    Stopwatch Program

---
+++ LogicalPrograms6/src/UC6_StopWatchProgram.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class UC6_StopWatchProgram {
+	public long startTimer=0;
+	public long stopTimer=0;
+	public long elapsed;
+	 void start()
+	{
+		startTimer=System.currentTimeMillis();
+		System.out.println("Start Time is: "+startTimer);
+	}	
+	void stop()
+
+	{
+		stopTimer=System.currentTimeMillis();
+		System.out.println("Stop Time is: "+stopTimer);
+	}
+
+
+	long getElapsedTime()
+	{
+		elapsed=stopTimer-startTimer;
+		return elapsed;
+    }
+public static void main(String[] args) {
+	UC6_StopWatchProgram sw=new UC6_StopWatchProgram();
+	Scanner inp=new Scanner(System.in);
+
+	System.out.println("Press '1' to Start Time: ");
+	int start=inp.nextInt();
+	sw.start();
+
+	System.out.println();
+	System.out.println("Press '2' to Stop Time: ");
+	int stop=inp.nextInt();
+	sw.stop();
+
+	long l=sw.getElapsedTime();
+	System.out.println();
+	System.out.println("Total Time Elapsed(in millisec) is:"+l);
+	System.out.println();
+	System.out.println("Converting millisec to seconds: "+(l/1000)+" sec");
+}
+}
